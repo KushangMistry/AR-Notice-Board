3fdb0ac364278dc7945250e851b513fb33ee80b4
KushangMistry <34236493+KushangMistry@users.noreply.github.com>
2019-01-11 21:36:28 +0530

    Add files via upload

---
+++ Assets/Vbbutton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vbbutton : MonoBehaviour {
+
+    
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        transferscript s1 = new transferscript();
+        string data = s1.setdata();
+        Debug.Log(data);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void clickbutton()
+    {
+        transferscript n1 = new transferscript();
+        n1.setdata();
+        SceneManager.LoadScene(1);
+    }
+    public void clickbutton2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void backButton()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1 | SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
